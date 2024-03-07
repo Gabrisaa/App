2dcf2ffb29a50548500224f094801e7c40a67e58
Gabrisaa <gabrielavilelaneto35@gmail.com>
2024-03-07 10:35:51 -0300

    trote de cria

--- src/screens/Slide1/index.tsx
+++ src/screens/Slide1/index.tsx
@@ -1,26 +1,37 @@
-import { View , Text} from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { styles } from './styles'
 
-export function Slide1(){
-    return (
-        <View style={styles.container}>
-            <View style={styles.bolomeio}>
 
-            </View>
-            <View style={styles.escrita}>
+export function Slide1() {
+  const bolinho = require("../../assets/bolo.png")
+  const planilha = require("../../assets/listinha.png")
+  const dinheiro = require("../../assets/dinheiro.png")
+  const coracao = require("../../assets/coracao.png")
+  const cinza = require("../../assets/coracaocinza.png")
+  return (
+    <View style={styles.container}>
+      <View style={styles.bolomeio}>
+        <Image source={bolinho} />
+      </View>
+      <View style={styles.escrita}>
+        <Text>Problemas com a Organização do seu evento?Deixa que o Organize Já te ajuda!</Text>
 
-            </View>
-            <View style={styles.imagenss}>
-                
-            </View>
-          <View style={styles.baixo} >
-            <Text>fjknfgjk</Text>
-            <Text>fjknfgjk</Text>
-            <Text>fjknfgjk</Text>
-            <Text>fjknfgjk</Text>
-            <Text>fjknfgjk</Text>
-          </View>
+      </View>
+      <View style={styles.imagenss}>
+        <View style={styles.dindin}>
+            <Image  source={planilha} />
+            <Image source={dinheiro} />
         </View>
-       )
-
+        <View style={styles.zirigui}> 
+          <Text> em listagens </Text>
+          <Text> e questões financeiras </Text>
+        </View>
+      <View style={styles.baixo} >
+            <Image  source={cinza} />
+            <Image source={coracao} />
+            <Image source={coracao} />
+      </View>
+    </View>
+    </View>
+  )
 }
--- src/screens/Slide1/styles.ts
+++ src/screens/Slide1/styles.ts
@@ -6,15 +6,26 @@ export const styles = StyleSheet.create({
         backgroundColor:'#F1D7D7',
     },
     bolomeio:{
-
+        marginVertical: 70,
+        alignItems:"center"
     },
     escrita:{
 
     },
     imagenss:{
-
+        alignItems:'center',
+    },
+    dindin:{
+        flexDirection:"row",
+        
+    },
+    zirigui:{
+        flexDirection: 'row',
+        textAlignVertical: 'center'
+        
     },
     baixo:{
+        marginVertical: 190,
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center'
