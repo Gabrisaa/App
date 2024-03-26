26e3ac1dd4285bc59a760d98e66b2aec463569da
Gabrisaa <gabrielavilelaneto35@gmail.com>
2024-03-26 13:10:54 -0300

    5

--- src/screens/Slide2/index.tsx
+++ src/screens/Slide2/index.tsx
@@ -1,10 +1,22 @@
-import { ImageBackground } from "react-native"
+import { ImageBackground, Text, View } from "react-native"
+import { Botaozinho } from "../../components/botaozinho"
 import { styles } from './styles'
 import { IPage } from "../../../App"
 
 export function Slide2({ Ipage, IsetPage }: IPage) {
   const slide = require('../../assets/fundo2.png')
+  const coracao = require("../../assets/coracao.png")
+  const cinza = require("../../assets/coracaocinza.png")
   return (
+    <>
         <ImageBackground style={styles.container} source={slide} />
+        <View>
+          <Text>Aqui você pode montar sua lista de convidados, fazer uma média de gastos e criar uma contagem regressiva </Text>
+          <View style={styles.baixo} >
+             <Botaozinho  Ipage={Ipage} IsetPage={IsetPage}/>           
+           </View>
+        </View>
+
+        </>
   )
 }
